780ae7b7cc5757ccf499438573f7af33754c1eff
kaiki <colaiiiyahhou@gmail.com>
2018-03-06 18:43:20 +0900

    睡眠判定実装

--- test.js
+++ test.js
@@ -2,9 +2,12 @@ var watson = require('watson-developer-cloud');
 var fs = require('fs');
 var Jimp = require("jimp");
 
+count = 0;
+
 main();
 
 function main(){
+  filename = "./test/image" + count + ".jpg";
   //子プロセスとしてカメラ起動
   const execSync = require('child_process').execSync;
   const result =  execSync('CommandCam.exe');
@@ -13,8 +16,15 @@ function main(){
   Jimp.read("image.bmp", function (err, lenna) {
       if (err) throw err;
         lenna.resize(256, 256)
-        .write("image.jpg"); // save 
+        .write("image.jpg"); // save
   });
+
+  Jimp.read("image.bmp", function (err, lenna) {
+      if (err) throw err;
+        lenna.resize(256, 256)
+        .write(filename); // save
+  });
+  count++;
 
   //watsonの資格情報
   var visual_recognition = watson.visual_recognition({
@@ -23,9 +33,9 @@ function main(){
     version_date: '2016-05-20'
   });
 
-  //どの分類樹に渡すか
+  //どの分類器に渡すか
   let parameters = {
-    classifier_ids: ["sleapjudge_126959574","default"],
+    classifier_ids: ["sleepjudge_121989147"],
     threshold: 0.6
   };
 
@@ -35,18 +45,60 @@ function main(){
     parameters: parameters
   };
 
+  const result2 =  execSync('start image.jpg');
+
   //結果出力
   visual_recognition.classify(params, function(err, response) {
+    //sleepjudge分類器に引っかかたか
+    if(response.images[0].classifiers[0]==null){
+      console.log("NO sleep")
+    }else{
+      //寝てるか、起きてるか
+      if(response.images[0].classifiers[0].classes[0].class == "sleep"){
+        if(response.images[0].classifiers[0].classes[0].score >= 0.5){
+          console.log("sleep!!!")
+        }
+      }
+    }
+    //watsonの結果を全部出力
     if (err)
       console.log(err);
     else
       console.log(JSON.stringify(response, null, 2))
   });
 
+  //var score = response.get("score");
+
   //一定時間ごとに起動するように
   setTimeout(function(){ 
         main();
     }, 10000);
+  
 }
+
+/*
+{
+  "images": [
+    {
+      "classifiers": [
+        {
+          "classifier_id": "sleepjudge_121989147",
+          "name": "sleepjudge",
+          "classes": [
+            {
+              "class": "sleep",
+              "score": 0.708816
+            }
+          ]
+        }
+      ],
+      "image": "sleep.jpg"
+    }
+  ],
+  "images_processed": 1,
+  "custom_classes": 2
+}
+*/
+
 
 //curl -X POST -F "images_file=@test.jpg" -F "parameters=@fruit.json" "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=8758c0fc3c232c775a7aa3d0873836f150614841&version=2016-05-20"
